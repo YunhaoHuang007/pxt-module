a93882be18ccf9f4d23acd247fd758f20e423401
Holly1999 <136292064+Holly1999@users.noreply.github.com>
2023-06-12 18:20:29 +0800

    更新 main.ts, _locales/zh/peripheral-strings.json

--- main.ts
+++ main.ts
@@ -1,45 +1,13 @@
 //% color="#CCFF33" weight=10 icon="\uf0ca" block="Peripheral:bit"
 namespace peripheral {
-    //% blockId=touchbutton block="touch |digital pin %pin"   group="触摸模块"
-    //% weight=70
-    //% subcategory="基础输入模块"
-    export function touchButton(pin: DigitalPin): boolean {
-        if (pins.digitalReadPin(pin) == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     //% blockId=button block="Button |digital pin %pin"   group="按键模块"
     //% weight=70
     //% subcategory="基础输入模块"
-    export function Button(pin: DigitalPin): boolean {
-        //   pins.digitalWritePin(pin, 0)
+    export function Button(pin: DigitalPin): boolean {        
         if (pins.digitalReadPin(pin) == 1) {
             return false;
         } else {
             return true;
         }
-    }
-
-    //% blockId=crashbutton block="crashButton |digital pin %pin"   group="触碰模块"
-    //% weight=70
-    //% subcategory="基础输入模块"
-    export function crashButton(pin: DigitalPin): boolean {
-        // pins.digitalWritePin(pin, 0)
-        if (pins.digitalReadPin(pin) == 1) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    //% blockId=slideRheostat block="slideRheostat |analog pin %pin"   group="滑动变阻器模块"
-    //% weight=70
-    //% subcategory="基础输入模块"
-    export function slideRheostat(pin: AnalogPin): number {
-        let row = pins.analogReadPin(pin)
-        return row
     }
 }

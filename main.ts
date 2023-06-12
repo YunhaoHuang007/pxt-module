aaa6255df24de4e9a0311c7c18d28aa04b35056e
Holly1999 <136292064+Holly1999@users.noreply.github.com>
2023-06-12 14:25:06 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-//% color="#FFA500" weight=10 icon="\uf2c9" block="Sensor:bit"
-namespace sensors {
+//% color="#FFA500" weight=10 icon="\uf2c9" block="Peripheral:bit"
+namespace peripheral {
     //% blockId=touchbutton block="touch |digital pin %pin"   group="触摸模块"
     //% weight=70
     //% subcategory="基础输入模块"
-    export function touchButton(pin: DigitalPin): boolean {
-        // pins.digitalWritePin(pin, 0)
+    export function touchButton(pin: DigitalPin): boolean {        
         if (pins.digitalReadPin(pin) == 1) {
             return true;
         } else {

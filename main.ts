706df0acef33dc13d92d07c656e3fa996c4d25c5
Holly1999 <136292064+Holly1999@users.noreply.github.com>
2023-06-12 12:23:31 +0800

    test1

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+//% color="#FFA500" weight=10 icon="\uf2c9" block="Sensor:bit"
+namespace sensors {
+    //% blockId=touchbutton block="touch |digital pin %pin"   group="触摸模块"
+    //% weight=70
+    //% subcategory="基础输入模块"
+    export function touchButton(pin: DigitalPin): boolean {
+        // pins.digitalWritePin(pin, 0)
+        if (pins.digitalReadPin(pin) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

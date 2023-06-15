220d9b9c1a50d047c065519bf3c987f2a00c0d6f
Holly1999 <136292064+Holly1999@users.noreply.github.com>
2023-06-15 19:22:28 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -263,6 +263,7 @@ namespace Module {
         pins.analogWritePin(pin, freq)
     }
 
+
     let COMMAND_I2C_ADDRESS = 0x24
     let DISPLAY_I2C_ADDRESS = 0x34
     let _SEG = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71]
@@ -270,100 +271,99 @@ namespace Module {
     let _intensity = 3
     let dbuf = [0, 0, 0, 0]
 
-    function cmd(c: number) {
-        pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, c, NumberFormat.Int8BE)
+    function TM1650SendCmd(tmcmd: number) {
+        pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, tmcmd, NumberFormat.Int8BE)
     }
 
-    function dat(bit: number, d: number) {
-        pins.i2cWriteNumber(DISPLAY_I2C_ADDRESS + (bit % 4), d, NumberFormat.Int8BE)
+    function TM1650SendData(bit: number, tmdata: number) {
+        pins.i2cWriteNumber(DISPLAY_I2C_ADDRESS + (bit % 4), tmdata, NumberFormat.Int8BE)
     }
 
-
-    //% blockId="TM650_Control" block="display control" group="TM1650数码管"
-    //% weight=40 blockGap=8
-    //% subcategory="显示器"
-    function TM650_Control(option: TM1650_OPT) {
-        if (option == 0) {
-            cmd(_intensity * 16 + 1)
+    //% subcategory="输出模块" group="TM1650数码管"
+    //% blockId=TM1650Control weight=47 blockGap=15
+    //% block="display control"
+    function TM1650Control(tmcmd: TM1650_OPT) {
+        if (tmcmd == 0) {
+            TM1650SendCmd(_intensity * 16 + 1)
         }
-        if (option == 1) {
+        if (tmcmd == 1) {
             _intensity = 0
-            cmd(0)
+            TM1650SendCmd(0)
         }
-        if (option == 2) {
-            dat(0, 0)
-            dat(1, 0)
-            dat(2, 0)
-            dat(3, 0)
+        if (tmcmd == 2) {
+            TM1650SendData(0, 0)
+            TM1650SendData(1, 0)
+            TM1650SendData(2, 0)
+            TM1650SendData(3, 0)
             dbuf = [0, 0, 0, 0]
         }
     }
 
-    //% blockId="TM650_DIGIT" block="show digit %num|at %bit"  group="TM1650数码管"
-    //% weight=80 blockGap=8
+    //% subcategory="输出模块" group="TM1650数码管"
+    //% blockId=TM1650Digit weight=80 blockGap=8
+    //% block="Bit %bit | show digit %num"
     //% num.max=15 num.min=0
-    //% subcategory="显示器"
     //% bit.max=3 bit.min=0
-    export function digit(num: number, bit: number) {
+    export function TM1650Digit(bit: number, num: number) {
         dbuf[bit % 4] = _SEG[num % 16]
-        dat(bit, _SEG[num % 16])
+        TM1650SendData(bit, _SEG[num % 16])
     }
 
-    //% blockId="TM650_SHOW_NUMBER" block="show number %num"  group="TM1650数码管"
-    //% weight=100 blockGap=8
-    //% subcategory="显示器"
-    export function showNumber(num: number) {
+    //% subcategory="输出模块" group="TM1650数码管"
+    //% blockId="TM1650ShowNumber" weight=100 blockGap=8    
+    //% block="show number %num"
+    export function TM1650ShowNumber(num: number) {
         if (num < 0) {
-            dat(0, 0x40) // '-'
+            TM1650SendData(0, 0x40) // '-'
             num = -num
         }
         else
-            digit(Math.idiv(num, 1000) % 10, 0)
-        digit(num % 10, 3)
-        digit(Math.idiv(num, 10) % 10, 2)
-        digit(Math.idiv(num, 100) % 10, 1)
+            TM1650Digit(Math.idiv(num, 1000) % 10, 0)
+        TM1650Digit(num % 10, 3)
+        TM1650Digit(Math.idiv(num, 10) % 10, 2)
+        TM1650Digit(Math.idiv(num, 100) % 10, 1)
     }
 
-    //% blockId="TM650_SHOW_HEX_NUMBER" block="show hex number %num"  group="TM1650数码管"
-    //% weight=90 blockGap=8
-    //% subcategory="显示器"
-    export function showHex(num: number) {
+    //% subcategory="输出模块" group="TM1650数码管"
+    //% block="show hex number %num"
+    //% blockId="TM1650ShowHex" weight=90 blockGap=8
+    export function TM1650ShowHex(num: number) {
         if (num < 0) {
-            dat(0, 0x40) // '-'
+            TM1650SendData(0, 0x40) // '-'
             num = -num
         }
         else
-            digit((num >> 12) % 16, 0)
-        digit(num % 16, 3)
-        digit((num >> 4) % 16, 2)
-        digit((num >> 8) % 16, 1)
+            TM1650Digit((num >> 12) % 16, 0)
+        TM1650Digit(num % 16, 3)
+        TM1650Digit((num >> 4) % 16, 2)
+        TM1650Digit((num >> 8) % 16, 1)
     }
 
-    //% blockId="TM650_SHOW_DP" block="show dot point %bit|show %num" group="TM1650数码管"
-    //% weight=80 blockGap=8
-    //% subcategory="显示器"
+    //% subcategory="输出模块" group="TM1650数码管"
+    //% blockId="TM1650ShowDp" weight=80 blockGap=8
+    //% block="show dot point %bit|show %status"
     //% bit.max=3 bit.min=0
-    export function showDpAt(status: LED_ON_OFF, bit: number) {
+    export function TM1650ShowDp(bit: number, status: LED_ON_OFF) {
         let show = status == 1 ? true : false;
-        if (show) dat(bit, dbuf[bit % 4] | 0x80)
-        else dat(bit, dbuf[bit % 4] & 0x7F)
+        if (show) TM1650SendData(bit, dbuf[bit % 4] | 0x80)
+        else TM1650SendData(bit, dbuf[bit % 4] & 0x7F)
     }
 
-    //% blockId="TM650_INTENSITY" block="set intensity %dat" group="TM1650数码管"
-    //% weight=70 blockGap=8
-    //% subcategory="显示器"
-    //% dat.max=7 dat.min=0
-    export function setIntensity(dat: number) {
-        if ((dat < 0) || (dat > 8)) {
+    //% subcategory="输出模块" group="TM1650数码管"
+    //% blockId=TM1650SetIntensity weight=70 blockGap=8
+    //% block="set intensity %value"
+    //% value.max=7 value.min=0
+    export function TM1650SetIntensity(value: number) {
+        if ((value < 0) || (value > 8)) {
             return
         }
-        if (dat == 0) {
+        if (value == 0) {
             _intensity = 0
-            cmd(0)
+            TM1650SendCmd(0)
         }
         else {
-            _intensity = dat
-            cmd((dat << 4) | 0x01)
+            _intensity = value
+            TM1650SendCmd((value << 4) | 0x01)
         }
     }
 }
